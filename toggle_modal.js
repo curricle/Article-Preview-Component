842e43b60541930883a01a9fba0635eb58ab7d3e
S. Jordan <curricle@gmail.com>
2025-04-10 15:18:14 -0400

    Finish mobile share popup styling

--- toggle_modal.js
+++ toggle_modal.js
@@ -1,4 +1,9 @@
 function toggleModal() {
   let popup = document.getElementById("share-popup");
+  let footerContent = document.getElementById("article-footer");
+  let sharePopupContent = document.getElementById("popup-content");
+  let defaultShareButton = document.getElementById("default-share-button");
   popup.classList.toggle("hidden");
+  footerContent.classList.toggle("reset-padding");
+  defaultShareButton.classList.toggle("hidden");
 }
