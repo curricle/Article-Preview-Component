342dbe1b0e811b60665cc6dba8805f6af1cee480
S. Jordan <curricle@gmail.com>
2025-04-10 15:24:46 -0400

    Update README.md

--- toggle_modal.js
+++ toggle_modal.js
@@ -1,8 +1,8 @@
 function toggleModal() {
   let popup = document.getElementById("share-popup");
   let footerContent = document.getElementById("article-footer");
-  let sharePopupContent = document.getElementById("popup-content");
   let defaultShareButton = document.getElementById("default-share-button");
+  
   popup.classList.toggle("hidden");
   footerContent.classList.toggle("reset-padding");
   defaultShareButton.classList.toggle("hidden");
